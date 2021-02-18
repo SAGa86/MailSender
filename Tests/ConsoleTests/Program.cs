1a299589fcb930a51694ee191eb66a92afac61d9
SAGa86 <slay_ag@mail.ru>
2021-02-18 13:50:13 +0300

    Корректировка кода, смена порта на 25, с указанного на сайте отправление не происходит.

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -21,11 +21,16 @@ namespace ConsoleTests
             // письмо представляет код html
             m.IsBodyHtml = true;
             // адрес smtp-сервера и порт, с которого будем отправлять письмо
-            var smtp = new SmtpClient("smtp.mail.com", 465);
+            var client = new SmtpClient("smtp.mail.ru", 25);
             // логин и пароль
-            smtp.Credentials = new NetworkCredential("slay_ag@mail.ru", "Bastardsaints96");
-            smtp.EnableSsl = true;
-            smtp.Send(m);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "slay_ag@mail.ru",
+                Password = "%%%"
+            };
+            
+            client.Send(m);
             Console.Read();
         }
     }
