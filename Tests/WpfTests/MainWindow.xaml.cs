a233f73b0ea8a229c639e06bd30a3b40808c9666
SAGa86 <slay_ag@mail.ru>
2021-02-18 13:59:49 +0300

    задали значения объектов на WPF, реализовали логику отправки сообщения.

--- Tests/WpfTests/MainWindow.xaml.cs
+++ Tests/WpfTests/MainWindow.xaml.cs
@@ -29,25 +29,37 @@ namespace WpfTests
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            // отправитель - устанавливаем адрес и отображаемое в письме имя
-            MailAddress from = new MailAddress("slay_ag@mail.com", "Alex");
-            // кому отправляем
-            MailAddress to = new MailAddress("saga281286@yandex.ru");
-            // создаем объект сообщения
-            MailMessage m = new MailMessage(from, to);
-            // тема письма
-            m.Subject = "ТестОтправки";
-            // текст письма
-            m.Body = "<h2>Письмо-тест работы smtp-клиента</h2>";
-            // письмо представляет код html
-            m.IsBodyHtml = true;
-            // адрес smtp-сервера и порт, с которого будем отправлять письмо
-            SmtpClient smtp = new SmtpClient("smtp.mail.com", 465);
-            // логин и пароль
-            smtp.Credentials = new NetworkCredential("slay_ag@mail.ru", "Bastardsaints96");
-            smtp.EnableSsl = true;
-            smtp.Send(m);
-            //Console.Read();
+            var from = new MailAddress("slay_ag@mail.ru", "Алексей Г.");
+            var to = new MailAddress("saga281286@yandex.ru", "Алексей Г.");
+
+            var message = new MailMessage(from, to);
+            message.Subject = "Заголовок";
+            message.Body = "Текст письма";
+
+            var client = new SmtpClient("smtp.mail.ru", 25);
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = LoginEdit.Text,
+                SecurePassword = PasswordEdit.SecurePassword
+            };
+
+            try
+            {
+                client.Send(message);
+
+                MessageBox.Show("Почта успешно отправлена!", "Отправка почты", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            catch (SmtpException)
+            {
+                MessageBox.Show("Ошибка авторизации", "Ошибка отправки почты", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (TimeoutException)
+            {
+                MessageBox.Show("Ошибка адреса сервера", "Ошибка отправки почты", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
