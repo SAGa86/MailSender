5e02161b896d821a989b5b4278535fa25fbd33a0
SAGa86 <slay_ag@mail.ru>
2021-02-18 13:08:55 +0300

    Перевод на .net5.0

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace ConsoleTests
 {
@@ -6,7 +8,25 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // отправитель - устанавливаем адрес и отображаемое в письме имя
+            MailAddress from = new MailAddress("slay_ag@mail.com", "Alex");
+            // кому отправляем
+            MailAddress to = new MailAddress("saga281286@yandex.ru");
+            // создаем объект сообщения
+            MailMessage m = new MailMessage(from, to);
+            // тема письма
+            m.Subject = "ТестОтправки";
+            // текст письма
+            m.Body = "<h2>Письмо-тест работы smtp-клиента</h2>";
+            // письмо представляет код html
+            m.IsBodyHtml = true;
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            SmtpClient smtp = new SmtpClient("smtp.mail.com", 465);
+            // логин и пароль
+            smtp.Credentials = new NetworkCredential("slay_ag@mail.ru", "Bastardsaints96");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+            Console.Read();
         }
     }
 }
--- Tests/WpfTests/MainWindow.xaml.cs
+++ Tests/WpfTests/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,29 @@ namespace WpfTests
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // отправитель - устанавливаем адрес и отображаемое в письме имя
+            MailAddress from = new MailAddress("slay_ag@mail.com", "Alex");
+            // кому отправляем
+            MailAddress to = new MailAddress("saga281286@yandex.ru");
+            // создаем объект сообщения
+            MailMessage m = new MailMessage(from, to);
+            // тема письма
+            m.Subject = "ТестОтправки";
+            // текст письма
+            m.Body = "<h2>Письмо-тест работы smtp-клиента</h2>";
+            // письмо представляет код html
+            m.IsBodyHtml = true;
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            SmtpClient smtp = new SmtpClient("smtp.mail.com", 465);
+            // логин и пароль
+            smtp.Credentials = new NetworkCredential("slay_ag@mail.ru", "Bastardsaints96");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+            //Console.Read();
         }
     }
 }
