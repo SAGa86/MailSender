322147d96f8d52381a1ccb24adc6f3022f13fabb
SAGa86 <slay_ag@mail.ru>
2021-02-22 19:53:01 +0300

    Модели данных

---
+++ Tests/WpfTests/Models/Recipient.cs
@@ -0,0 +1,9 @@
+﻿namespace WpfTests.Models
+{
+    public class Recipient
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ Tests/WpfTests/Models/Server.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfTests.Models
+{
+    public class Server
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Port { get; set; }
+        public bool UseSSL { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+    }
+}
