202fda1e213f8ab87d22cc0fa9b60274ac84fb5e
SAGa86 <slay_ag@mail.ru>
2021-02-22 20:58:12 +0300

    Дизайн тестовых списков, серверов, отправителей, биндинг к элементам WPF

---
+++ Tests/WpfTests/Data/TestData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfTests.Models;
+
+namespace WpfTests.Data
+{
+    internal static class TestData
+    {
+        public static List<Server> Servers { get; } = Enumerable.Range(1, 10)
+            .Select(i => new Server 
+            {
+                Name = $"Сервер - {i}",
+                Address = $"smtp.server{i}.com",
+                Login = $"Login - {i}",
+                Password = $"Password - {i}",
+                UseSSL = i % 2 == 0,
+            })
+            .ToList();
+
+        public static List<Sender> Senders { get; } = Enumerable.Range(1, 10)
+            .Select(i => new Sender
+            {
+                Name = $"Отправитель - {i}",
+                Address = $"sender_{i}@server.com",              
+            })
+            .ToList();
+
+        public static List<Recipient> Recipients { get; } = Enumerable.Range(1, 10)
+           .Select(i => new Recipient
+           {
+               Name = $"Получатель - {i}",
+               Address = $"recipient_{i}@server.com",
+           })
+           .ToList();
+
+        public static List<Message> Messages { get; } = Enumerable.Range(1, 10)
+           .Select(i => new Message
+           {
+               Title = $"Тема - {i}",
+               Body = $"Текст сообщения {i}",
+           })
+           .ToList();
+    }
+}
