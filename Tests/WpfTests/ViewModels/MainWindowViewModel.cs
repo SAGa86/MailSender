5b476d4b1386bc154f8ffa47b1851e0f6048f1a2
SAGa86 <slay_ag@mail.ru>
2021-02-25 22:00:04 +0300

    Модель - представление окна WpfTests

---
+++ Tests/WpfTests/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MailSender.lib.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTests.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Рассыльщик";
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+        private string _Status = "Готов!";
+        public string Status { get => _Status; set => Set(ref _Status, value); }
+
+    }
+}
