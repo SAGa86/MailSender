b4317507cf2dd21c49bc4156f743135dbafcdc76
SAGa86 <slay_ag@mail.ru>
2021-02-25 22:45:25 +0300

    Контейнер сервисов

--- Tests/WpfTests/App.xaml.cs
+++ Tests/WpfTests/App.xaml.cs
@@ -1,17 +1,33 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfTests.ViewModels;
 
 namespace WpfTests
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    
+    public partial class App
     {
+        private static IHost __Hosting;
+        public static IHost Hosting => __Hosting
+            ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+        .CreateDefaultBuilder(args).ConfigureAppConfiguration(opt => opt.AddJsonFile("appsettings.json", false, true))
+        .ConfigureServices(ConfigureServices)
+        ;
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
     }
 }
