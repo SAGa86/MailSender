97cb1029a007415590e53ecd2370582ba98f385f
SAGa86 <slay_ag@mail.ru>
2021-02-25 19:13:58 +0300

    Заготовка собственного элемента управления

---
+++ Tests/WpfTests/Controls/ItemPanel.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTests.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для ItemPanel.xaml
+    /// </summary>
+    public partial class ItemPanel : UserControl
+    {
+        public string Title { get; set; }
+        public ItemPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
