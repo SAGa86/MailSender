0f18acda6a676835da6d9b39c9c793a782d26c30
SAGa86 <slay_ag@mail.ru>
2021-03-07 15:48:39 +0300

    Создание класса для работы с сущностями RepositoryInMemory + переписан класс ServersRepository.

--- Tests/WpfTests/Infrastructure/Services/InMemory/ServersRepository.cs
+++ Tests/WpfTests/Infrastructure/Services/InMemory/ServersRepository.cs
@@ -5,18 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using WpfTests.Models;
 using WpfTests.Infrastructure.Services.InMemory;
+using System.Windows.Documents;
 
 namespace WpfTests.Infrastructure.Services.InMemory
 {
-    class ServersRepository : IRepository<Server>
+    class ServersRepository : RepositoryInMemory<Server>
     {
-        private static List<Server> _Servers;
+        //private static List<Server> _Servers;
 
-        private int _MaxId;
+        //private int _MaxId;
 
-        public ServersRepository()
-        { 
-             _Servers = new List<Server> 
+        public ServersRepository() : base(new List<Server>
              {
                  new Server
                  {
@@ -38,38 +37,17 @@ namespace WpfTests.Infrastructure.Services.InMemory
                      Login = "slay_ag@mail.ru",
                      Password = "Bastard_saints96",
                  },
-             };
-            _MaxId = _Servers.Max(s => s.Id);
+             })
+        {
+
         }
 
-        public IEnumerable<Server> GetAll() => _Servers;
-
-        public int Add(Server item)
+        public override void Update(Server item)
         {
-            //_Servers.Add(server);
-            if (_Servers.Contains(item))
-                return item.Id;
-            item.Id = ++_MaxId;
-            _Servers.Add(item);
-            return item.Id;
-        }
-
-        //public void Remove (Server server)
-        //{
-        //    _Servers.Remove(server);
-        //}
-
-        public Server GetById(int id) => _Servers.FirstOrDefault(s => s.Id == id);
-        
-
-       
-
-        public void Update(Server item)
-        {
-            if (_Servers.Contains(item))
-                return;
+            //if (_Servers.Contains(item))
+            //    return;
             var db_item = GetById(item.Id);
-            if (db_item is null) return;
+            if (db_item is null || ReferenceEquals(db_item, item)) return;
 
             db_item.Name = item.Name;
             db_item.Address = item.Address;
@@ -79,9 +57,5 @@ namespace WpfTests.Infrastructure.Services.InMemory
             db_item.Port = item.Port;
         }
 
-        public bool Remove(int id)
-        {
-            return _Servers.RemoveAll(s => s.Id == id) > 0;
-        }
     }
 }
