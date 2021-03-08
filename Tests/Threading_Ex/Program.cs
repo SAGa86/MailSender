48435efaee7a14203f0a986d05bd0da1fb3fe13f
SAGa86 <slay_ag@mail.ru>
2021-03-09 00:36:31 +0300

    ДЗ 5. Мультипоток. Решение в нэймспейсе Threading_Ex

---
+++ Tests/Threading_Ex/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading_Ex
+{
+    
+    class Program
+    {
+        //private static object lockObject = new object();
+
+        static long HalfFact(long l, long r)
+        {
+            if (l > r)
+                return 1;
+            if (l == r)
+                return l;
+            if (r - l == 1)
+                return (long)l * r;
+            long m = (l + r) / 2;
+            var tskFac1 = Task.Factory.StartNew<long>(() => HalfFact(l, m));
+            var tskFac2 = Task.Factory.StartNew<long>(() => HalfFact(m + 1, r));
+            Task.WaitAll(tskFac1, tskFac2);
+            return tskFac1.Result*tskFac2.Result;
+        }
+
+        static long FactoReal(long n)
+        {
+            if (n < 0)
+                return 0;
+            if (n == 0)
+                return 1;
+            if (n == 1 || n == 2)
+                return n;
+            return HalfFact(2, n);
+        }
+
+        static long Sum(long sum, long digit)
+        {
+            long halfSum = sum / 2;
+            
+            return sum;
+
+        }
+        static void Main()
+        {
+            long N = 0;
+            Console.WriteLine("Введите число N");
+            if (long.TryParse(Console.ReadLine(), out N) && N > 0)
+                Console.WriteLine($"Факториал {N} равен {FactoReal(N)}");
+            else
+                Console.WriteLine("Вы ввели не число!");
+            Console.ReadKey();
+
+        }
+        
+    }
+}
---
+++ Tests/Threading_Ex/threadClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading_Ex
+{
+    public class threadClass
+    {
+        private int _sleepTime;
+        private string _message;
+        public threadClass(int sleepTime, string message)
+        {
+            _sleepTime = sleepTime;
+            _message = message;
+        }
+        public void ThreadClassMethod()
+        {
+            Thread.Sleep(_sleepTime);
+            Console.WriteLine(_message);
+        }
+
+    }
+}
