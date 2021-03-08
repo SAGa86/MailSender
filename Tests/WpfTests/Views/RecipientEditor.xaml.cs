b55c29e2153444a59f42a483ee3a3118b2544833
SAGa86 <slay_ag@mail.ru>
2021-03-08 13:00:09 +0300

    Отображение всплывающих подсказок.

--- Tests/WpfTests/Views/RecipientEditor.xaml.cs
+++ Tests/WpfTests/Views/RecipientEditor.xaml.cs
@@ -18,11 +18,38 @@ namespace WpfTests.Views
     /// <summary>
     /// Логика взаимодействия для RecipientEditor.xaml
     /// </summary>
-    public partial class RecipientEditor : UserControl
+    public partial class RecipientEditor
     {
         public RecipientEditor()
         {
             InitializeComponent();
+        }
+
+        private void OnNameValidationError(object sender, ValidationErrorEventArgs e)
+        {
+            if(e.Action == ValidationErrorEventAction.Added) 
+            {
+                ((Control)sender).ToolTip = e.Error.ErrorContent.ToString();
+            }
+            else
+            {
+                ((Control)sender).ClearValue(ToolTipProperty);
+            }
+
+        }
+
+        private void OnIdValidationError(object sender, ValidationErrorEventArgs e)
+        {
+
+            if (e.Action == ValidationErrorEventAction.Added)
+            {
+                ((Control)sender).ToolTip = e.Error.ErrorContent.ToString();
+            }
+            else
+            {
+                ((Control)sender).ClearValue(ToolTipProperty);
+            }
+
         }
     }
 }
