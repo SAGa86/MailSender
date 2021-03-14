66b1945693837c673044674c96a1c3144b45a960
SAGa86 <slay_ag@mail.ru>
2021-03-14 21:02:19 +0300

    Задание 6.

---
+++ SyncVsAsyncWPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace SyncVsAsyncWPF
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       /* static Task<int> MethodAsync(int x)
+        {
+            int result = 1;
+
+            return Task.Run(() =>
+            {
+                Thread.Sleep(10000);
+                result = x * x;
+                return result;
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                    int num = 5;
+                    label.Content = MethodSync(num).ToString();
+            }
+
+        static int MethodSync(int x)
+        {
+            int result = 1;
+
+            Thread.Sleep(10000);
+            result = x * x;
+            return result;
+        }
+       */
+    }
+}
+
+
