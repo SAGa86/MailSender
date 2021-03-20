b6529c2db2c24c01be4240dffe918a9cf6306959
SAGa86 <slay_ag@mail.ru>
2021-03-20 17:06:30 +0300

    Создали тестовый проект и папку с моделями

---
+++ TestConsoleDB/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestConsoleDB
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
