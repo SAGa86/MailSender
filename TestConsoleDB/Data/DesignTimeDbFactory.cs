cbd9092546a8f95b8b04ce85f7d0cde1a74c0dec
SAGa86 <slay_ag@mail.ru>
2021-03-21 20:01:33 +0300

    тестовый консольный DB проект и начало работы с основным

---
+++ TestConsoleDB/Data/DesignTimeDbFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleDB.Data
+{
+    class DesignTimeDbFactory
+    {
+        public StudentsDb CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<StudentsDb>()
+               .UseLazyLoadingProxies()
+               .UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog=Students.DB")
+               .Options;
+
+            return new StudentsDb(options);
+        }
+    }
+}
--- TestConsoleDB/Program.cs
+++ TestConsoleDB/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
+using TestConsoleDB.Data;
+using TestConsoleDB.Entities;
 
 namespace TestConsoleDB
 {
@@ -7,7 +11,54 @@ namespace TestConsoleDB
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var options = new DbContextOptionsBuilder<StudentsDb>()
+               .UseLazyLoadingProxies()
+               .UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog=Students.DB")
+               .Options;
+
+            
+
+
+            using (var db = new StudentsDb(options))
+                await InitializeDBAsync(db);
+        }
+
+        private static async Task InitializeDBAsync(StudentsDb db)
+        {
+            await db.Database.EnsureDeletedAsync();
+            await db.Database.EnsureCreatedAsync();
+            //await db.Database.MigrateAsync();
+
+            if (!await db.Courses.AnyAsync())
+            {
+                var courses = Enumerable.Range(1, 10)
+                   .Select(i => new Course { Name = $"Предмет {i}" })
+                   .ToArray();
+
+                await db.Courses.AddRangeAsync(courses);
+
+                await db.SaveChangesAsync();
+
+                if (!await db.Students.AnyAsync())
+                {
+                    var rnd = new Random();
+                    var students = Enumerable.Range(1, 1000)
+                       .Select(
+                            i => new Student
+                            {
+                                Name = $"Имя-{i}",
+                                LastName = $"Фамилия-{i}",
+                                Patronymic = $"Отчество-{i}",
+                                Birthday = DateTime.Now.Date.AddYears(-rnd.Next(17, 28)).AddDays(rnd.Next(365)),
+                                Courses = Enumerable.Range(0, rnd.Next(1, 8))
+                                   .Select(_ => courses[rnd.Next(courses.Length)])
+                                   .Distinct()
+                                   .ToArray()
+                            });
+                    await db.Students.AddRangeAsync(students);
+                    await db.SaveChangesAsync();
+                }
+            }
         }
     }
 }
